b56d15c56882c4a07c7bb8e7829c7b9620412241
Sravan Tallapaka <sravtall@gmail.com>
2023-07-07 12:03:19 -0700

    initial stage/set up files and react

---
+++ README.md
@@ -0,0 +1,15 @@
+Website
+
+things to include
+
+- main page
+
+  - pic + short intro about self
+  - involvements
+  - projects
+
+- about page + contact
+  - bit more about myself
+  - goals + aspirations
+  - socials + ways to contact
+  - other misc (could be other page)
---
+++ website-current/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import MainPage from "./pages/MainPage/MainPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+      <AboutPage />
+    </div>
+  );
+}
+
+export default App;
