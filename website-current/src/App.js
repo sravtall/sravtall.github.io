44450cdf0005c2e05134bc2948941085dee61194
Sravan Tallapaka <sravtall@gmail.com>
2023-09-06 15:03:58 -0700

    updating redirects 404.html plus hashrouter

--- website-current/src/App.js
+++ website-current/src/App.js
@@ -1,5 +1,5 @@
 import { React } from "react";
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes, HashRouter as Router } from "react-router-dom";
 
 import MainPage from "./pages/MainPage/MainPage";
 import AboutPage from "./pages/AboutPage/AboutPage";
